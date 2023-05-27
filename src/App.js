952c2b1e279a91452df83fbab6af833b97ac0bae
Roland550 <reoutaderoland@gmail.com>
2023-05-27 16:05:42 +0100

    first comit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+import Header from './components/header/Header'
+import Contact from './components/contact/contact';
+
+function App() {
+  return (
+    <>
+      <Header/>
+      <Contact/>
+    </>
+  );
+}
+
+export default App;
